a1796aece52ea897eda47255c22eb269c6190db4
K0NSTRUCT0R <andrey.kavetskiy@icloud.com>
2023-04-19 20:48:08 +0300

    доделал третью задачу

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+int Read()
+{
+    System.Console.WriteLine("Введите число:");
+    int x = int.Parse(Console.ReadLine());
+    if (x < 0) throw new ArgumentOutOfRangeException("x");
+    return x;
+}
+
+int x = Read();
+
+void Magic(int x)
+{
+    int a = 1;
+    int b = 0;
+    while (a <= x)
+    {
+        b = (int)Math.Pow(a, 3);
+        a++;
+        System.Console.Write(b + " ");
+    }
+    
+}
+
+Magic(x);
